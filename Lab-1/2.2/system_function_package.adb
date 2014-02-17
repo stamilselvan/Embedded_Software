8ee9aca84fd51014843f6c63d60a4e16ec5c8bbf
stamilselvan <yestamilselvan@gmail.com>
2014-02-17 19:18:34 +0100

    Lab 2 & Edits

--- Lab-1/2.2/system_function_package.adb
+++ Lab-1/2.2/system_function_package.adb
@@ -1,90 +1,21 @@
-with Ada.Text_IO, Ada.Integer_Text_IO;
-use Ada.Text_IO, Ada.Integer_Text_IO;
+package body System_Function_Package is
 
-with Ada.Real_Time;
-use Ada.Real_Time;
-
-with System_Function_Package;
-use System_Function_Package;
-
-procedure Cyclic_Scheduler is
-   X : Integer := 0; -- Input
-   Y : Integer := 0; -- Input
-   result_X : Integer; -- Output from System_X
-   result_Y : Integer; -- Output from System_Y
-
-   task Source_X;
-   task Source_Y;
-   task Scheduler;
-
-   task body Source_X is -- Generates Data for Input X
-      Start : Time;
-      Next_Release : Time;
-      Release : Time_Span := Milliseconds(0);
-      Period : Time_Span := Milliseconds(1000);
+   function System_A(X : Integer) return Integer is
    begin
-      Start := Clock;
-      Next_Release := Start + Release;
-      loop
-         delay until Next_Release;
-         Next_Release:= Next_Release + Period;
-         X := X + 1;
-         New_Line;
-         Put("Compute X ");
-         result_X := System_Function_Package.System_A(X);
-         New_Line;
-         Put("X Executed ");
-         Put(Duration'Image(To_Duration(Release)));
-         New_Line;
-      end loop;
-   end Source_X;
+      delay To_Duration(Milliseconds(100));
+      return X + 1;
+   end System_A;
 
-   task body Source_Y is -- Generated Data for Input Y
-      Start : Time;
-      Next_Release : Time;
-      Release: Time_Span := Milliseconds(500);
-      Period : Time_Span := Milliseconds(1000);
+   function System_B(Y : Integer) return Integer is
    begin
-      Start := Clock;
-      Next_Release := Start + Release;
-      loop
-         delay until Next_Release;
-         Next_Release:= Next_Release + Period;
-         Y := Y + 1;
-         New_Line;
-         Put("Compute Y ");
-         result_Y := System_Function_Package.System_B(Y);
-         New_Line;
-         Put("Y Executed :");
-         Put(Duration'Image(To_Duration(Release)));
-         New_Line;
-      end loop;
-   end Source_Y;
+      delay To_Duration(Milliseconds(200));
+      return Y * 2;
+   end System_B;
 
-   task body Scheduler is
-      result_Z : Integer; -- Output from System_Z
-      Start : Time;
-      Next_Release : Time;
-      Release : Time_Span := Milliseconds(750);
-      Period : Time_Span := Milliseconds(1000);
+   function System_C(X, Y : Integer) return Integer is
    begin
+      delay To_Duration(Milliseconds(200));
+      return X * Y;
+   end System_C;
 
-      Start := Clock;
-      Next_Release := Start + Release;
-      loop
-         delay until Next_Release;
-         Next_Release := Next_Release + Period;
-         New_Line;
-         Put("Compute Z ");
-         result_Z := System_Function_Package.System_C(result_X, result_Y);
-         New_Line;
-         Put("Output is Z : ");
-         Put(result_Z);
-         New_Line;
-      end loop;
-
-   end Scheduler;
-
-begin
-   null;
-end Cyclic_Scheduler;
+end System_Function_Package;
